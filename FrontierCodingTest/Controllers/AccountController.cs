9510eb0c27426d402676432f33e1ccb61a4e8372
AdamWhite <adamwhitehat@outlook.com>
2019-03-21 05:00:34 -0600

    Added some comments discussing some of the choices I made regarding the view, and my thoughts in that area.

--- FrontierCodingTest/Controllers/AccountController.cs
+++ FrontierCodingTest/Controllers/AccountController.cs
@@ -15,6 +15,12 @@ namespace FrontierCodingTest.Controllers
             {
                 List<AccountModel> accounts = await ControllerHelper.HttpGetDataAsync<List<AccountModel>>();
 
+                // It was tempting to put the linq where clauses directly in the view, thereby avoiding having to create this container model.
+                // However, that would technically be breaking the MVC pattern by putting logic in the view.
+                // The rule of thumb here is, is to imagine the view will be handled by UX or graphic designer people,
+                // and is unreasonable to expect them to deal with linq, session state or any kind of logic really.
+                // Even the foreach loops that I did put in the view is pushing it.
+                // I kept all the diplay formatting logic in the model and out of the view for the same reasons.
                 AccountsContainerModel collectionModel = new AccountsContainerModel();
                 collectionModel.ActiveAccounts = accounts.Where(accnt => accnt.AccountStatusId == AccountStatus.Active).ToList();
                 collectionModel.OverdueAccounts = accounts.Where(accnt => accnt.AccountStatusId == AccountStatus.Overdue).ToList();
